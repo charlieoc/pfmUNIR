da59f7aafb11dcd60816bf4efbdb383af3d566e0
Roberto Ortiz <19339094+charlieoc@users.noreply.github.com>
2021-06-15 18:48:05 -0500

    Modificación al 15/06/2021

--- analisis_sentimiento.py
+++ analisis_sentimiento.py
@@ -1,14 +1,30 @@
-import mysql.connector as msql
-import pandas as pd
+from nltk import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
 
-# Crear el query
-query = "SELECT fechapub, contenido FROM pfm_conferencias order by fechapub asc"
+# Archivo de texto a utilizar como fuente
+abrir_archivo = "/home/robert/GitHub/pfmUNIR/archivos/ConferenciasMatutinas.txt"
 
-# Conectar con la BD
-conn = msql.connect(user="conferencias", password="i^]D4vJ[$M$5r,6<Kh~F", host="127.0.0.1", port=3306, database="conferencias")
+with open(abrir_archivo, 'r') as file:
+    text = file.read()
 
-# Crear el DataFrame con el resultado del query
-df = pd.read_sql(query, conn)
+# Separar palabras
+tokens = word_tokenize(text)
 
+# Transformar a minúsculas
+tokens = [w.lower() for w in tokens]
 
+# Quitar puntuación
+table = str.maketrans('', '', string.punctuation)
+stripped = [w.translate(table) for w in tokens]
 
+# Quitar otros caracteres no alfanuméricos
+words = [word for word in stripped if word.isalpha()]
+
+# Quitar palabras comunes (stop words)
+stop_words = set(stopwords.words('spanish'))
+words = [w for w in words if not w in stop_words]
+
+freq_dist = FreqDist(words)
+
+freq_dist.plot(25, cumulative=False)
